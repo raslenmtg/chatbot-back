af8dae0d911557dc383fbb99630e73132fcac3bb
Raslen Maatoug <r.maatoug@n-3rd.com>
2019-11-06 12:30:54 +0100

    change routes security

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -19,35 +19,14 @@ class AuthentificationController extends AbstractController
     const C_TOKEN_FAILURE = 'Failed to get JWT Token :';
 
     /**
-     * @Route("/login",name="login", methods={"POST"})
+     * @Route("/api/login_check",name="login", methods={"POST"})
      */
-    public function Login(Request $i__request, UserService $i__userService, UserPasswordEncoderInterface $i__encoder)
-    {
-        try {
-            /*
-            * Get Request body content (username, password, device)
-            */
-                $l__username = $i__request->get("username");
-            $l__password = $i__request->get("password");
-            $l__isValid = $i__userService->toCheckCredentials($i__encoder, $l__username, $l__password);
-            if ($l__isValid =='invalid') {
-                //Bad Credentials
-                $l__response = array('authenticated' => 0);
-            } else {
-                //Correct credentials
-                $l__user = $l__isValid;
-                $l__token = $i__userService->toGenerateToken($l__user);
-                $l__response = array('token' => $l__token, 'username' => $l__user->getUsername(), 'authenticated' => 1);
-            }
-        } catch (Exception $e) {
-            throw new UnexpectedValueException(AuthentificationController::C_TOKEN_FAILURE . $e->getMessage());
-        }
-        return new JsonResponse($l__response);
-    }
+    public function login_check()
+    {}
 
 
     /**
-     * @Route("/chatbot",name="chatbot", methods={"POST"})
+     * @Route("/api/chatbot",name="chatbot", methods={"POST"})
      */
     public function ChatbotService(Request $request, ChatbotService $chatbotService)
     {
