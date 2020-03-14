23462786e82740e43b5db153e4c1e2fabc24ded7
Raslen Maatoug <r.maatoug@n-3rd.com>
2020-03-14 22:16:06 +0100

    prod

--- src/Controller/ChatbotController.php
+++ src/Controller/ChatbotController.php
@@ -26,17 +26,17 @@ class ChatbotController extends AbstractController
      */
     public function ChatbotService(Request $request, ChatbotService $chatbotService): Response
     {
-/*
+
         $response = new MessagingResponse();
         $data = array('message' => $request->get('Body'), 'phone_number' => substr($request->get('From'), 9));
-        $answer = $chatbotService->typeofmessage($data);
+        $answer = $chatbotService->typeofmessage_it($data);
         $response->message($answer);
         return Response::create($response, 200, array());
-*/
 
+/*
         $data = array('message' => $request->get('Body'), 'phone_number' => substr($request->get('From'), 9));
         $answer = $chatbotService->typeofmessage_it($data);
-        return Response::create($answer, 200, array());
+        return Response::create($answer, 200, array());*/
     }
 
 
